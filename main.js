770bdfbba571d4c57040f57074716fddcbb025c4
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-10-04 14:29:32 +0500

    start hw object

---
+++ main.js
@@ -0,0 +1,16 @@
+// let person = new Object();
+// let person2 = {};
+
+
+// let person = {
+//     name:"Shahrom",
+//     age:17,
+// }
+// console.log(person)
+// // {name: 'Shahrom', age: 17}
+
+
+let person = {
+    name:"Shahrom",
+    age:17,
+}
