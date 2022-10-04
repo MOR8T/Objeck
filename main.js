9bd39194073468a9e426b0106fef48169135c00b
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-10-04 14:48:48 +0500

    commit2

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+// // #1
 // let person = new Object();
 // let person2 = {};
 
-
+// // #2
 // let person = {
 //     name:"Shahrom",
 //     age:17,
@@ -9,8 +10,35 @@
 // console.log(person)
 // // {name: 'Shahrom', age: 17}
 
+// // #3
+// let person = {
+//     name:"Shahrom",
+//     age:17,
+//     adres:{
+//         city: "Dushanbe",
+//         district: "Sino",
+//         strit: "Asadulo Gulomov 28",
+//         kv: 20,
+//     }
+// }
+// console.log(person);
+// console.log(person.name);
+// console.log(person.adres);
+// console.log(person.adres.city);
 
+
+// // #4
 let person = {
     name:"Shahrom",
     age:17,
+    adres:{
+        city: "Dushanbe",
+        district: "Sino",
+        strit: "Asadulo Gulomov 28",
+        kv: 20,
+    }
 }
+console.log(person);
+console.log(person.name);
+console.log(person.adres);
+console.log(person.adres.city);
